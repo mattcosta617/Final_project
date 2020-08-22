096e41d35a8f7c80912afcd8717c91265dc7bc41
mattcosta617 <mattiecosta@gmail.com>
2020-08-21 20:07:20 -0400

    Initial Setup complete

---
+++ find-an-expert/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './component/Home/Home'
+
+function App(props) {
+  return (
+    <div className="App">
+       <Home />
+    </div>
+  );
+}
+
+export default App;
