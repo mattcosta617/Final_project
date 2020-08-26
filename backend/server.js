47b89c1535209a80c6db9c26199cce922d922ca9
mattcosta617 <mattiecosta@gmail.com>
2020-08-25 22:55:51 -0400

    duplicate

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // Routes defined
 experts = require("./routes/experts");
